ca90d58b8f407d0e4b2f477f949e8d5e8affe756
mfcoral <2342@holbertonschool.com>
2020-09-18 22:11:27 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ *main - prints a string of character.
  *
- *Return: Always 0 (Success)
+ *Return: Always 0 (success)
  */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
